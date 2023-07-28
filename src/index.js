bc1e51eee40b315a7eff42cc8ccf2ca6554596db
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2023-07-28 13:36:33 -0700

    /new-bucket endpoint actual writes bucket folder to filesystem now.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,18 @@ app.use(express.json());
 const EXPRESS_PORT = 3000;
 
 app.post('/new-bucket', (req, res)=> {
-    res.send({message:`Created bucket with the name: ${req.body.bucket}`});
 
+    // Remember: This path is from app/ as thats the Docker working directory
+    fs.mkdir(`store/${req.body.bucket}`, (err) => {
+        if (err) {
+          console.error('Error creating directory:', err);
+          res.send({message:`Error creating bucket`});
+          // How do I deal with errors in express again lol? Server status?
+        } else {
+          console.log('Directory created successfully!');
+          res.send({message:`Created bucket with the name: ${req.body.bucket}`});
+        }
+      });
 })
 
 app.post('/create-file', (req, res)=> {
