6812266b7667caa1e3ae000378d3a735f36312dc
AntonDmitriev1484 <a.dmitriev@wustl.edu>
2023-07-31 10:53:46 -0700

    File transfer being a painright now, I'll do this later.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from "express"
 import fs from "fs"
+import multer from "multer"
+import busboy from "connect-busboy"
 
 const app = express();
 app.use(express.json());
+app.use(busboy());
 const EXPRESS_PORT = 3000;
+const upload = multer({ dest: 'store/' });
 
 app.post('/new-bucket', (req, res)=> {
 
@@ -20,7 +24,31 @@ app.post('/new-bucket', (req, res)=> {
       });
 })
 
-app.post('/create-file', (req, res)=> {
+app.post('/create-file', upload.single('file'), (req, res)=> {
+  //console.log(req.body);
+
+  req.pipe(req.busboy);
+  req.busboy.on('file', (fieldname, file, filename) => {
+    console.log("Uploading: " + filename);
+    //Path where image will be uploaded
+    fstream = fs.createWriteStream('store/water.csv');
+    file.pipe(fstream);
+    fstream.on('close', function () {    
+        console.log("Upload Finished of " + filename);              
+    }
+    );
+});
+
+    // fs.writeFile(`store/${req.body.bucket}/${req.body.path}`, file, (err) => {
+    //     if (err) {
+    //       console.error('Error creating file', err);
+    //       res.send({message:`Error creating file`});
+    //       // How do I deal with errors in express again lol? Server status?
+    //     } else {
+    //       console.log('File created successfully!');
+    //       res.send({message:`Created file in bucket ${req.body.bucket} at ${req.body.path}.`});
+    //     }
+    //   });
     
 })
 
